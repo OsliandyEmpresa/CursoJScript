14edc9544012764e33fa506b6bcb5a6d895615c5
OsliandyEmpresa <osliandyempresa@gmail.com>
2025-06-11 04:56:01 +0200

    HOla

---
+++ Basico/03-operadores.js
@@ -0,0 +1,64 @@
+//Operadores
+
+//Atritmeticos
+
+let a = 10
+let b = 5
+
+console.log(a + b) //suma
+console.log(a - b) //resta
+console.log(a * b) //multiplicacion
+console.log(a / b) //divicion
+console.log(a % b) //modulo
+console.log(a ** b) //potencia
+
+a++
+console.log(a) //incremento
+
+b--
+console.log(b) //decremento
+
+//Asignacion
+
+let v = 2
+v +=2
+v -=2
+v *=2
+v /=2
+v %=2
+v **=2
+
+//Comparacion
+
+console.log( 1 > 10)
+console.log( 1 < 10)
+console.log( 1 >= 10)
+console.log( 1 <= 10)
+console.log( 1 != 10)
+console.log( 1 !== 10)
+console.log( 1 == 10) //Igualdad por valor
+console.log( 1 === 10)//Igualdad valor identidad
+
+//Logicos
+
+// and (&&)
+console.log(5 > 10 && 10 < 20)
+
+// or (||)
+console.log(5 > 10 || 10 < 20)
+
+// not (!)
+console.log(!(5 > 10 || 10 < 20))
+
+Ternario
+const hola = true
+
+//si la variable es tree se realiza lo de alante de los puntos sino lo de atras
+hola ? console.log("hola") : console.log("yes")
+
+
+
+
+
+
+
